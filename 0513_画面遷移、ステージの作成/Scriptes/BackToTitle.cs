4d111c3419ec826f295f00903a4b39c3aad58525
Yoshizumi Toru <skyno50925@gmail.com>
2020-05-13 16:19:47 +0900

    キャラクター選択画面作成、ステージ作成(途中まで)

---
+++ 0513_画面遷移、ステージの作成/Scriptes/BackToTitle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BackToTitle : MonoBehaviour
+{
+    public void Onclick()
+    {
+        //ここで移りたいシーンを指定
+        SceneManager.LoadScene("Title");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
