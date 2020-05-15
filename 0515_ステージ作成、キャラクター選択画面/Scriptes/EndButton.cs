53739a86bfee049f80bb1fdf5cb725bb186e2f7a
Yoshizumi Toru <skyno50925@gmail.com>
2020-05-15 17:31:46 +0900

    キャラクター選択画面でのスクリプトの変更、ステージの基礎的な部分の完成　吉住

---
+++ 0515_ステージ作成、キャラクター選択画面/Scriptes/EndButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndButton : MonoBehaviour
+{
+    public void Onclick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying=false;
+#else
+        Application.Quit();
+#endif
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
