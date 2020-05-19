a3a2385d124322d6c3c7f386b6203e605eef4845
Yoshizumi Toru <skyno50925@gmail.com>
2020-05-19 16:10:08 +0900

    宝箱のアセット、開閉のアニメーション、プレイヤーとの当たり判定で開くようにするスクリプト

---
+++ 0519_今日の作業/Scriptes/Explanation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Explanation : MonoBehaviour
+{
+    public void Onclick()
+    {
+        SceneManager.LoadScene("Option");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 0519_今日の作業/Scriptes/TreasureBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureBox : MonoBehaviour
+{
+    private Animator animator;
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            animator = GetComponent<animator>();
+            animator.SetBool("touch", true);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
