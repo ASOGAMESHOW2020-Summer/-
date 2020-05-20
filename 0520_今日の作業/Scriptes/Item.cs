791dba5c3b4b1e0d4a19015e5aeff9c0c98f4837
Yoshizumi Toru <skyno50925@gmail.com>
2020-05-20 17:50:50 +0900

    Itemマネージャー、itemクラスの作成のスクリプト

---
+++ 0520_今日の作業/Scriptes/Item.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+[CreateAssetMenu(fileName = "Item", menuName = "CreateItem")]
+public class Item : ScriptableObject
+{
+    public enum KindOfItem
+    {
+        Weapon,
+        UseItem
+    }
+
+    //アイテムの種類
+    [SerializeField]
+    private KindOfItem kindOfItem;
+    //アイテムのアイコン
+    [SerializeField]
+    Sprite icon;
+    //アイテムの名前
+    [SerializeField]
+    private string itemName;
+    //アイテムの情報
+    [SerializeField]
+    private string information;
+
+    public KindOfItem GetKindOfItem()
+    {
+        return kindOfItem;
+    }
+
+    public sprite GetIcon()
+    {
+        return icon;
+    }
+
+    public string GetItemName()
+    {
+        return itemName;
+    }
+
+    public string GetInformation()
+    {
+        return information;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
