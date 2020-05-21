46fb1dce66399b4e390a2fce38e1da31ebc7e360
Yoshizumi Toru <skyno50925@gmail.com>
2020-05-21 16:23:57 +0900

    ドアのスクリプトの追加、宝箱からアイテムが出てくるスクリプト

---
+++ 0521_今日の作業/Scriptes/Gizmo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(255, 0, 0, 0.7f);
+        Gizmos.DrawCube(transform.position, this.transform.localScale);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
