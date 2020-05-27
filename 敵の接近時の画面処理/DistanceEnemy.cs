a35b17dc9b52199b1d834e6a4cd79f0d05e61afd
GOLGO (馬場) <winaz.1999@gmail.com>
2020-05-27 15:33:00 +0900

    敵が近づいてきたときわかりやすく画面に変化を加える（今のところはUIでImageを表示）　*途中まで　馬場 全体での表示はできているので後は個人個人で表示するだけ

---
+++ 敵の接近時の画面処理/DistanceEnemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceEnemy : MonoBehaviour
+{
+    //自身との距離を計算するターゲットオブジェ
+    [SerializeField]
+    private Transform targetObj;
+    //コライダーを考慮したオフセット値
+    private float colliderOffset;
+    //テスト用UI
+    [SerializeField]
+    private Text distanceUI;
+    [SerializeField]
+    private Flash flash;
+    [SerializeField]
+    private GameObject obj;
+
+   
+
+    void Start()
+    {
+        colliderOffset = GetComponent<CharacterController>().radius + targetObj.GetComponent<CharacterController>().radius;
+        obj = GameObject.Find("FlashImage");
+        flash = obj.GetComponent<Flash>();
+    }
+
+    void Update()
+    {
+        //距離を計算
+       // var distance = Vector3.Distance(transform.position, targetObj.position) - colliderOffset;
+        
+        if(Vector3.Distance(transform.position, targetObj.position) - colliderOffset < 5f)
+        {
+            flash.ImgFlash(true);
+        }
+        else if(Vector3.Distance(transform.position, targetObj.position) - colliderOffset > 5f)
+        {
+            flash.ImgFlash(false);
+        }
+        //if(distanceUI != null)
+        //{
+        //    distanceUI.text = distance.ToString("0.00m");
+        //}
+        //else
+        //{
+        //    Debug.Log(distance.ToString("0.00m"));
+        //}
+    }
+}
---
+++ 敵の接近時の画面処理/Flash.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Flash : MonoBehaviour
+{
+    [SerializeField]
+    Image img;
+
+    void Awake()
+    {
+        img = GetComponent<Image>();
+        img.color = Color.clear;
+    }
+
+
+    public void ImgFlash(bool flag)
+    {
+        if (flag == true)
+        {
+            //徐々に赤色にする
+            this.img.color = Color.Lerp(this.img.color, Color.red, Time.deltaTime);
+        }
+        else if (flag == false)
+        {
+            //徐々に透明にする
+            this.img.color = Color.Lerp(this.img.color, Color.clear, Time.deltaTime);
+
+        }
+    }
+}
