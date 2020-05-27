1ef2c17d5650652bb40379d2764c46d51bebc064
Yoshizumi Toru <skyno50925@gmail.com>
2020-05-27 15:49:10 +0900

    クリア、ゲームシステムなどのスクリプトの変更、ステージの一部の変更　途中多　　吉住

---
+++ 0527_今日の作業/SamuraiButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamuraiButton : MonoBehaviour
+{
+    [SerializeFiled]
+    public GameObject Samurai;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Samurai = GameObject.Find("侍");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  
+}
