fdbae89b8af67eed2155e0e1505914976b13835c
GOLGO(馬場) <winaz.1999@gmail.com>
2020-05-28 12:55:14 +0900

    インスペクター上でUIのImageのサイズと位置をhwン甲できるようにした。あとはカメラの分割モードに合わせるだけ　馬場

---
+++ 敵の接近時の画面処理/P2DistanceEnemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class P2DistanceEnemy : MonoBehaviour
+{
+    //自身との距離を計算するターゲットオブジェ
+    [SerializeField]
+    private Transform targetObj;
+    //コライダーを考慮したオフセット値
+    private float colliderOffset;
+    //テスト用UI
+    [SerializeField]
+    private Text distanceUI;
+    [SerializeField]
+    private GameObject FlashObj;
+    [SerializeField]
+    private Flash flash;
+    private RectTransform FlashImage;
+    public float w, h;//変更したいサイズ
+    public float x, y, z;//変更したい座標
+
+    void Start()
+    {
+        colliderOffset = GetComponent<CharacterController>().radius + targetObj.GetComponent<CharacterController>().radius;
+        FlashObj = GameObject.Find("P2FlashImage");
+        flash = FlashObj.GetComponent<Flash>();
+        FlashImage = GameObject.Find("P2FlashImage").GetComponent<RectTransform>();
+        FlashImage.sizeDelta = new Vector2(w, h);
+        FlashImage.localPosition = new Vector3(x, y, z);
+    }
+
+    void Update()
+    {
+        //距離を計算
+        var distance = Vector3.Distance(transform.position, targetObj.position) - colliderOffset;
+
+        if (Vector3.Distance(transform.position, targetObj.position) - colliderOffset < 5f)
+        {
+            flash.ImgFlash(true);
+        }
+        else if (Vector3.Distance(transform.position, targetObj.position) - colliderOffset > 5f)
+        {
+            flash.ImgFlash(false);
+        }
+        if (distanceUI != null)
+        {
+            distanceUI.text = distance.ToString("0.00m");
+        }
+        else
+        {
+            Debug.Log(distance.ToString("0.00m"));
+        }
+    }
+}
