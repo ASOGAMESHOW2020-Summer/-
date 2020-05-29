c0101a75d42cabdd1cb3f5eacb372670f9d24df3
Yoshizumi Toru <skyno50925@gmail.com>
2020-05-29 15:22:55 +0900

    クリア時のスクリプトなど

---
+++ 0529_今日の作業/MonkButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkButton : MonoBehaviour
+{
+    [SerializeFiled]
+    public GameObject Monk;
+
+    [SerializeFiled]
+    public GameObjet PlayerButton3;
+
+    public void OnClick()
+    {
+        Monk = GameObject.Find("僧侶");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 0529_今日の作業/SamuraiButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamuraiButton : MonoBehaviour
+{
+    [SerializeFiled]
+    public GameObject Samurai;
+
+    [SerializeFiled]
+    public GameObject PlayerButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (OnClick.PlayerButton==true)
+        {
+            Samurai = GameObject.Find("侍");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  
+}
