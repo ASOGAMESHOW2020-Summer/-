695c49ef2a8c7c1fc4387c6b21c04d36ca303a8d
GOLGO <winaz.1999@gmail.com>
2020-05-29 16:15:07 +0900

    ゲームクリア時の処理とそれに使うテスト用の鍵オブジェクト　（未完成）　馬場

---
+++ ゲームクリア処理/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //プレイヤー（侍）を格納する
+    [SerializeField]
+    private GameObject Samurai;
+    [SerializeField]
+    private SamuraiMoveScript SamuraiScript;
+    //プレイヤー（僧侶）を格納する
+    [SerializeField]
+    private GameObject Monk;
+    [SerializeField]
+    private MonkMoveScript MonkScript;
+    //プレイヤー（陰陽師）を格納する
+    [SerializeField]
+    private GameObject Onmyoji;
+    [SerializeField]
+    private OnmyojiMoveScript OnmyojiScript;
+    //プレイヤー（盗賊）を格納する
+    [SerializeField]
+    private GameObject Thief;
+    [SerializeField]
+    private ThiefMoveScript ThiefScript;
+    ////鍵を格納する
+    //[SerializeField]
+    //private GameObject KeyObj;
+    //[SerializeField]
+    //private Key KeyScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Samurai = GameObject.Find("侍");
+        SamuraiScript = Samurai.GetComponent<SamuraiMoveScript>();
+        Monk = GameObject.Find("僧侶");
+        MonkScript = Monk.GetComponent<MonkMoveScript>();
+        Onmyoji = GameObject.Find("陰陽師");
+        OnmyojiScript = Onmyoji.GetComponent<OnmyojiMoveScript>();
+        Thief = GameObject.Find("盗賊");
+        ThiefScript = Thief.GetComponent<ThiefMoveScript>();
+        Debug.Log("とんでもねぇ待ってたんだ");
+        //KeyObj = GameObject.Find("鍵");
+        //KeyScript = KeyObj.GetComponent<Key>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       GameOver();
+    }
+
+    void GameOver()
+    {
+        //プレイヤー（侍）の状態を取得
+        SamuraiMoveScript.SamuraiState SamuraiState = SamuraiScript.GetState();
+        //プレイヤー（僧侶）の状態を取得
+        MonkMoveScript.MonkState MonkState = MonkScript.GetState();
+        //プレイヤー（陰陽師）の状態を取得
+        OnmyojiMoveScript.OnmyojiState OnmyojiState = OnmyojiScript.GetState();
+        //プレイヤー（盗賊）の状態を取得
+        ThiefMoveScript.ThiefState ThiefState = ThiefScript.GetState();
+
+        if (SamuraiState == SamuraiMoveScript.SamuraiState.Dead
+        && (MonkState == MonkMoveScript.MonkState.Dead)
+        && (OnmyojiState == OnmyojiMoveScript.OnmyojiState.Dead)
+        && (ThiefState == ThiefMoveScript.ThiefState.Dead))
+        {
+            SceneManager.LoadScene("Test");
+            Debug.Log("ゲームオーバシーン移動");
+        }
+    }
+}
---
+++ ゲームクリア処理/Key.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Key : MonoBehaviour
+{
+    private int KeyNum = 0;
+    private const int GetKeyMax = 2;
+    public Text text;
+
+    void Start()
+    {
+        KeyNum = 0;
+    }
+    void Update()
+    {
+        text.text = KeyNum.ToString();
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.tag == "Samurai")
+        {
+            Destroy(gameObject);
+            coll.GetComponent<SamuraiMoveScript>().SetKeyFlag(true);
+            KeyNum += 1;
+        }
+        else if(coll.tag == "Monk")
+        {
+            Destroy(gameObject);
+            coll.GetComponent<MonkMoveScript>().SetKeyFlag(true);
+            KeyNum += 1;
+        }
+        else if(coll.tag == "Onmyoji")
+        {
+            Destroy(gameObject);
+            coll.GetComponent<OnmyojiMoveScript>().SetKeyFlag(true);
+            KeyNum += 1;
+        }
+        else if(coll.tag == "Thief")
+        {
+            Destroy(gameObject);
+            coll.GetComponent<ThiefMoveScript>().SetKeyFlag(true);
+            KeyNum += 1;
+        }
+    }
+
+    public int GetKeyNum()
+    {
+        return KeyNum;
+    }
+
+    public void GameClear()
+    {
+        if (KeyNum >= GetKeyMax)
+        {
+            SceneManager.LoadScene("Test2");
+            Debug.Log("ゲームクリアシーン移動");
+        }
+    }
+}
