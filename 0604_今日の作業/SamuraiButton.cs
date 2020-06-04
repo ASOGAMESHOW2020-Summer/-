c883b65f0353358c39e0d3de87380bcf780c66ec
toruSIF378 <49849074+toruSIF378@users.noreply.github.com>
2020-06-04 17:28:55 +0900

    キャラクター選択ボタンスクリプトの変更、侍ボタンスクリプトの変更、スクリプトがコンポーネントできない不具合修正

---
+++ 0604_今日の作業/SamuraiButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamuraiButton : MonoBehaviour
+{
+    public enum SamuraiButtonState
+    {
+        Select,
+        NoSelect
+    };
+    private CharacterButton characterButton;
+
+    private Animator animator;
+
+    [SerializeFiled]
+    public GameObject Samurai;
+
+    [SerializeFiled]
+    public GameObject PlayerButton;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<animator>();
+        if (OnClick.PlayerButton==true)
+        {
+            Samurai = GetComponent<CharacterSelect>();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  
+}
